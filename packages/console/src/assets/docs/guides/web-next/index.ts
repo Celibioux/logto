dd9b69dbd9f36854d365dd001e83ff4c6e83e87a
wangsijie <wangsijie@silverhand.io>
2024-05-14 13:25:30 +0800

    fix(console): remove outdated nextjs app router guide
    fixed #5820

--- packages/console/src/assets/docs/guides/web-next/index.ts
+++ packages/console/src/assets/docs/guides/web-next/index.ts
@@ -3,15 +3,13 @@ import { ApplicationType } from '@logto/schemas';
 import { type GuideMetadata } from '../types';
 
 const metadata: Readonly<GuideMetadata> = Object.freeze({
-  name: 'Next.js',
-  description:
-    'Next.js is a React framework for production - it makes building fullstack React apps a breeze and ships with built-in SSR.',
+  name: 'Next.js (Pages Router)',
+  description: 'Next.js integration guide for Pages Router.',
   target: ApplicationType.Traditional,
   sample: {
     repo: 'js',
     path: 'packages/next-sample',
   },
-  isFeatured: true,
   fullGuide: {
     title: 'Full Next.js SDK tutorial',
     url: 'https://docs.logto.io/sdk/next',
